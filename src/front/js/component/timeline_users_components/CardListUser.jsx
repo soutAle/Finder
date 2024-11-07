e4ae3ef980abf1e6e459b00509d5b7cf72b48e29
soutAle <alevalencigiraldo@gmail.com>
2024-11-07 22:00:46 +0100

    estilando timeline user

--- src/front/js/component/timeline_users_components/CardListUser.jsx
+++ src/front/js/component/timeline_users_components/CardListUser.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import { useLoadUsers } from "../../hooks/useLoadUsers.jsx";
 import { CardUserPremiumLogic } from "../timeline_users_components/CardUserPremiumLogic.jsx";
 
-
 export const CardListUser = () => {
     const { users = [], loading } = useLoadUsers();
 
     return (
-        <div className="list-user-container d-flex">
+        <div className="container my-4">
             {loading ? (
-                <p className="loading-section text-center">Cargando ofertas...</p>
+                <p className="loading-section text-center">Cargando usuarios...</p>
             ) : Array.isArray(users) && users.length > 0 ? (
-                users.map((user) => (
-                    <div key={user.id}>
-                        <CardUserPremiumLogic user={user} />
-                    </div>
-                ))
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+                    {users.map((user) => (
+                        <div className="col d-flex" key={user.id}>
+                            <div className="w-100">
+                                <CardUserPremiumLogic user={user} />
+                            </div>
+                        </div>
+                    ))}
+                </div>
             ) : (
                 <p className="no-users-section text-center text-secondary shadow-lg">
                     No hay usuarios disponibles
--- src/front/js/component/timeline_users_components/CardUser.jsx
+++ src/front/js/component/timeline_users_components/CardUser.jsx
@@ -1,22 +1,24 @@
 import React, { useContext } from "react";
 import { Context } from "../../store/appContext";
 import { useNavigate, Link } from "react-router-dom";
+import "../../../styles/card-user.css"
 
 
 export const CardUser = ({ user }) => {
 
     return (
-        <div className="card mx-3" style={{ width: "18rem" }}>
-            <img src={user.img} className="card-img-top" alt="profile image" />
-            <div className="card-body">
-                <h5 className="card-title">{user.name}</h5>
-                <h3 className="card-role">{user.role}</h3>
-                <span className="card-country">{user.country}</span>
-                <p className="card-text">{user.description}</p>
-                <Link to={`/profiledeveloper/${user.id}`} className="btn btn-primary">
+        <div className="card m-3 card-user-container">
+            <img src="https://media.istockphoto.com/id/1152447561/es/foto/empresario-sentado-en-su-escritorio-y-el-uso-de-la-computadora-port%C3%A1til-en-la-oficina.jpg?s=612x612&w=0&k=20&c=yupSG4Xwurwy65tkuDpUOttlz2AOH4kA6SMWts_bIBE=" className="card-img-top-user" alt="profile image" />
+            <div className="card-user-body p-2">
+                <h5 className="card-user-title">{user.name} {user.last_name}</h5>
+                <span className="card-user-country">{user.country}</span>
+                <h3 className="card-user-role">{user.role}</h3>
+                <span className="card-user-telephone">{user.telephone}</span>
+                <p className="card-user-text">{user.description}</p>
+                <Link to={`/profiledeveloper/${user.id}`} className="btn btn-sm btn-primary me-2">
                     Ver Perfil
                 </Link>
-                <button className="btn btn-success">Contactar</button>
+                <button className="btn btn-sm btn-success">Contactar</button>
             </div>
         </div>
     )
