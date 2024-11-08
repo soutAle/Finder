4654b71cad27ded5671b816c9b78b9b850ddd6a0
soutAle <alevalencigiraldo@gmail.com>
2024-11-08 03:45:28 +0100

    Vistas

---
+++ src/front/js/pages/MyJobApplies.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const MyJobApplies = () => {
+    return (
+        <div className="container mt-5 text-center">
+            <h1>Mis Candidaturas</h1>
+        </div>
+    )
+}
---
+++ src/front/js/pages/MyOffersPosted.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+
+export const MyOffersPosted = () => {
+
+    return (
+        <div className="container mt-5 text-center">
+            <h1>Mis Ofertas</h1>
+        </div>
+    )
+}
