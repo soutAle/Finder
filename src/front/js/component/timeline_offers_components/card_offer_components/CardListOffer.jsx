a72d8834138fca9fb70f1e62f0dfe65390122925
soutAle <alevalencigiraldo@gmail.com>
2024-11-08 17:55:18 +0100

    svg

--- src/front/js/component/timeline_offers_components/card_offer_components/CardListOffer.jsx
+++ src/front/js/component/timeline_offers_components/card_offer_components/CardListOffer.jsx
@@ -7,7 +7,18 @@ export const CardListOffers = () => {
     return (
         <div className="list-offer-container mt-3 m-auto">
             {loading ? (
-                <p className="loading-section text-center">Cargando ofertas...</p>
+                <div className="svg-box">
+                    <svg className="svg-load-icon" fill="hsl(228, 97%, 42%)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="4" cy="12" r="3" opacity="1">
+                            <animate id="spinner_qYjJ" begin="0;spinner_t4KZ.end-0.25s" attributeName="opacity" dur="0.75s" values="1;.2" fill="freeze" />
+                        </circle>
+                        <circle cx="12" cy="12" r="3" opacity=".4">
+                            <animate begin="spinner_qYjJ.begin+0.15s" attributeName="opacity" dur="0.75s" values="1;.2" fill="freeze" />
+                        </circle><circle cx="20" cy="12" r="3" opacity=".3">
+                            <animate id="spinner_t4KZ" begin="spinner_qYjJ.begin+0.3s" attributeName="opacity" dur="0.75s" values="1;.2" fill="freeze" />
+                        </circle>
+                    </svg>
+                </div>
             ) : Array.isArray(offers) && offers.length > 0 ? (
                 offers.map((offer) => (
                     <div key={offer.id}>
