faa973035f6264013efb9d8a26028433d5b51369
soutAle <alevalencigiraldo@gmail.com>
2025-01-02 15:45:24 +0100

    keyframe hero-section && ActionButtons

--- src/front/js/component/home_components/HeroSection.jsx
+++ src/front/js/component/home_components/HeroSection.jsx
@@ -8,13 +8,19 @@ export const HeroSection = () => {
     return (
         <section className="hero-section d-flex justify-content-center align-items-center my-5">
             <div className="text-center m-0 my-5">
-                {store?.user && (
+                {store?.user ? (
                     <>
-                        <h1 className="greeting-user-home display-4 mb-4">Bienvenido de nuevo {store?.user?.name}</h1>
+                        <h1 className="greeting-user-home display-4 mb-5">Bienvenido de nuevo {store?.user?.name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <h1 className="greeting-user-home display-4 mb-5">¡Bienvenido a Finder!</h1>
                     </>
                 )}
-                <h1 className="display-4">Conecta Desarrolladores con Empresas</h1>
-                <h2 className="lead mb-5">Encuentra tu próximo proyecto o el talento ideal para tu equipo</h2>
+                <div className="title-container">
+                    <h1 className="first-title-home display-4">Conecta Desarrolladores con Empresas</h1>
+                    <h2 className="lead mb-5">Encuentra tu próximo proyecto o el talento ideal para tu equipo</h2>
+                </div>
                 <Link to="/signup" className="btn btn-hero btn-lg mt-3 rounded-pill">
                     ¡Comienza Ahora!
                 </Link>
--- src/front/js/component/timeline_offers_components/card_offer_components/ActionButtons.jsx
+++ src/front/js/component/timeline_offers_components/card_offer_components/ActionButtons.jsx
@@ -5,12 +5,15 @@ import { Link } from 'react-router-dom';
 
 export const ActionButtons = ({ offer_id }) => {
     const { store, actions } = useContext(Context);
-    const [isSubscribed, setIsSubscribed] = useState(null);
+    const [isSubscribed, setIsSubscribed] = useState(false);
 
-    useEffect(() => {
-        const checkSubscription = store.user?.candidates?.some((candidate) => candidate.id === offer_id);
-        setIsSubscribed(checkSubscription);
-    }, [offer_id, store.user?.candidates]);
+
+    if (store.token) {
+        useEffect(() => {
+            const checkSubscription = store.user?.candidates?.some((candidate) => candidate.id === offer_id);
+            setIsSubscribed(checkSubscription);
+        }, [offer_id, store.user?.candidates]);
+    }
 
     const handleSubscription = async () => {
         if (!isSubscribed) {
