4fd0cb6490e9b767e0c56590a97fddef17a6d74b
soutAle <alevalencigiraldo@gmail.com>
2025-01-12 00:22:22 +0100

    About section WhyUs

--- src/front/js/component/about_components/HeroSectionAbout.jsx
+++ src/front/js/component/about_components/HeroSectionAbout.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import '../../../styles/about.css' // Asegúrate de tener un archivo CSS para estilos personalizados
+import { WhyUsAbout } from "./WhyUsAbout.jsx";
 
 
 export const HeroSectionAbout = ({ title, subtitle, phrases }) => {
     return (
-        <section className="container-fluid about-page text-center">
-            <div className="row">
-                <div className="col-4 col-md-6 col-sm-12 p-4 about-hero">
-                    <div className="title-box">
-                        <h1 className="title-hero fw-lighter">{title}</h1>
-                        <h2 className="subtitle-hero fw-lighter">{subtitle}</h2>
-                    </div>
-                    <div className="text-container mt-4">
-                        {phrases.map((phrase, index) => (
-                            <p key={index}>{phrase}</p>
-                        ))}
-                    </div>
+        <section className="about-page d-flex">
+            <div className="col-6 p-4 about-hero">
+                <div className="title-box">
+                    <h1 className="title-hero fw-lighter">{title}</h1>
+                    <h2 className="subtitle-hero fw-lighter">{subtitle}</h2>
+                </div>
+                <div className="text-container mt-4">
+                    {phrases.map((phrase, index) => (
+                        <p key={index}>{phrase}</p>
+                    ))}
                 </div>
             </div>
         </section>
